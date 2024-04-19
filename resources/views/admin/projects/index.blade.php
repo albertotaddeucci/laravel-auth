497d1b9e5330182f7ec55d986de19a603082264e
alberto <alberto.taddeucci@gmail.com>
2024-04-19 10:48:12 +0200

    feat: aggiunta pagina show

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -7,6 +7,7 @@
         <li class="list-group-item">
             <div class="row">
                 <div class="col">{{$project->title}}</div>
+                <div class="col text-end"><a href="{{route('admin.projects.show', $project->id)}}">Esplora</a></div>
             </div>
         </li>        
         @endforeach
--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -1,1 +1,66 @@
-show
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container py-5">
+    
+    <div class="row align-items-center">
+        <div class="col text-center">
+            <img src="{{$project->thumb}}" class="img-fluid" style="width: 200px" alt="">
+        </div>
+        <div class="col text-center">
+            <h2>{{$project->title}}</h2>
+
+        </div>
+    </div>
+    <hr>
+    
+    <div class="row justify-content-center py-5">
+        <div class="col-6">
+            <p>{{$project->description}}</p>
+        </div>
+        <div class="col-6 text-center">
+            
+            <a href="{{route('admin.projects.edit', $project->id)}}" class="btn btn-warning me-3">Modifica</a>
+            <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                Elimina
+            </button>
+
+        </div>
+    </div>
+
+
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">Elimina</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
+            <div class="modal-body">
+              Sei sicuro di voler eliminare questo progetto?
+            </div>
+
+
+            <div class="modal-footer">
+
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                <form action="{{route('admin.projects.destroy', $project->id)}}" method="POST">
+                    @csrf
+                    @method("DELETE")                    
+                    
+                    <button class="btn btn-danger">Elimina</button>
+                </form>
+
+            </div>
+
+          </div>
+        </div>
+    </div>
+
+</div>
+    
+@endsection
